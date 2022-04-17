6d8f9d3f09093232ccd71e1fe0cefb884ea98b04
JadenKim-dev <jaden.kim5179@gmail.com>
2022-04-17 21:17:19 +0900

    docker-compose

--- docker-compose-app/server.js
+++ docker-compose-app/server.js
@@ -15,10 +15,10 @@ client.set("number", 0);
 app.get('/', (req, res) => {
     client.get("number", (err, number) => {
         // 현재 숫자를 가져온 후에 1씩 올려줍니다.
-        client.set("number", parseInt(number) + 1);
         res.send("숫자가 1씩 올라갑니다. 숫자: " + number);
-    })
-})
+        client.set("number", parseInt(number) + 1);
+    });
+});
 
 app.listen(8080);
 console.log('Server is running');
